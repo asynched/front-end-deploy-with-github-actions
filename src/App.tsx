39bef991742d9c518859f6d4972088d8d9847c4b
Eder Lima <lima.eder101@gmail.com>
2022-10-18 05:17:22 -0300

    feat: add animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 import { useBox } from '@/lib/box'
 
 import { gameBox } from '@/stores/game'
@@ -10,13 +12,28 @@ export default function App() {
   const { board, winner, status, currentPlayer: player } = game
 
   return (
-    <main className="grid place-items-center w-full h-screen text-zinc-600">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold tracking-tighter text-zinc-900">
-          Tic-tac-toe
-        </h1>
-        <div className="text-center">
-          <p className="mb-2">
+    <main className="grid place-items-center w-full h-screen text-zinc-600 bg-gradient-to-br from-purple-600 to-indigo-600">
+      <motion.div
+        initial={{ y: -64, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-col gap-4 bg-white rounded-2xl shadow-xl px-24 py-12"
+      >
+        <motion.h1
+          initial={{ y: -64, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.125, duration: 0.5 }}
+          className="text-center text-6xl font-bold tracking-tighter bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text text-transparent"
+        >
+          TicTacToe
+        </motion.h1>
+        <motion.div
+          initial={{ x: -64, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.25, duration: 0.5 }}
+          className="text-center"
+        >
+          <p className="mb-2 text-xl">
             Status: {status}
             {winner && (
               <>
@@ -25,31 +42,49 @@ export default function App() {
               </>
             )}
           </p>
-          <button className="text-sm border py-1 px-4" onClick={reset}>
+          <button
+            className="border py-1 rounded px-8 transition ease-in-out hover:border-transparent hover:text-white hover:bg-purple-600"
+            onClick={reset}
+          >
             Reset
           </button>
-        </div>
-        <div className="grid self-center">
+        </motion.div>
+        <div className="mt-4 grid gap-4 self-center">
           {board.map((row, rowIndex) => (
-            <div className="flex" key={rowIndex}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.25 + rowIndex * 0.125, duration: 0.5 }}
+              className="flex gap-4"
+              key={rowIndex}
+            >
               {row.map((column, columnIndex) => (
-                <Square
-                  handleClick={() => {
-                    if (status === 'win') {
-                      return
-                    }
-
-                    setPlayer(rowIndex, columnIndex, player)
-                    validateGame(game)
+                <motion.div
+                  initial={{ opacity: 0, x: -24 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{
+                    delay: 0.25 + rowIndex * 0.125 + columnIndex * 0.125,
+                    duration: 0.5,
                   }}
                   key={columnIndex}
-                  type={column}
-                />
+                >
+                  <Square
+                    handleClick={() => {
+                      if (status === 'win') {
+                        return
+                      }
+
+                      setPlayer(rowIndex, columnIndex, player)
+                      validateGame(game)
+                    }}
+                    type={column}
+                  />
+                </motion.div>
               ))}
-            </div>
+            </motion.div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </main>
   )
 }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion'
 import type { Player } from '@/domain/entities'
+import { XMarkIcon, StopIcon } from '@heroicons/react/24/outline'
 
 type SquareProps = {
   type: Player
@@ -7,11 +9,22 @@ type SquareProps = {
 
 export default function Square({ type, handleClick }: SquareProps) {
   return (
-    <button
+    <motion.button
+      key={type}
+      initial={{ opacity: 0, y: -24 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 24 }}
+      transition={{ duration: 0.25 }}
       onClick={handleClick}
-      className="text-2xl border w-16 h-16 hover:bg-gray-100"
+      className="grid place-items-center w-16 h-16 border rounded"
     >
-      {type}
-    </button>
+      {type === 'X' ? (
+        <XMarkIcon className="w-12 h-12 text-purple-600" />
+      ) : type === 'O' ? (
+        <StopIcon className="w-12 h-12 text-indigo-600" />
+      ) : (
+        ''
+      )}
+    </motion.button>
   )
 }
